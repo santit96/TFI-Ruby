6b53b2d14342996606f28e49297fa3d849f5f519
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2017-12-20 19:53:38 -0300

    empece a testear

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
 	has_many :students , dependent: :restrict_with_error
 	has_many :evaluations , dependent: :restrict_with_error
 	validates :name , uniqueness: {scope: :year , message: "Shouldn't be the same course twice in a year"}
+	validates :year , numericality: {only_integer:true , greater_than: 1.years.ago.year}
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,12 @@
-require 'test_helper'
+#require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create course" do
+  	c=Course.new name:"Ruby" , year:2017
+  	assert_equal(c.save,true)	
+  end
 end
