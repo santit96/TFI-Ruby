f823352dc332b0104e9d35f0392938a35a3ecbc5
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2017-12-20 15:48:13 -0300

    hecho el seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+c1=Course.create(name: "Ruby",year:"2017",updated_at:Date.today,created_at:Date.today)
+c2=Course.create(name: "Ruby",year:"2018",updated_at:Date.today,created_at:Date.today)
+10.times do |i|
+	Student.create(name:"Estudiante #{i}",lastname:"Universitario", dni:38670800+i,number:i,updated_at:Date.today,created_at:Date.today,course_id:[c1.id,c2.id].sample)
+
+end
+3.times do |i|
+Evaluation.create(min_grade:4,course_id:c1.id, name: "Fecha #{i}",date:'2017-12-25',updated_at:Date.today,created_at:Date.today)
+Evaluation.create(min_grade:6,course_id:c2.id, name: "Fecha #{i}",date:Date.tomorrow,updated_at:Date.today,created_at:Date.today)
+
+end
+c1.students.each{|s| 
+	c1.evaluations.each{|e|
+	Grade.create(student_id:s.id,grade:rand(10).to_s,evaluation_id:e.id )}
+}
+c2.students.each{|s| 
+	c2.evaluations.each{|e|
+	Grade.create(student_id:s.id,evaluation_id:e.id )}
+}
