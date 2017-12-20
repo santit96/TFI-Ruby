49f7b5d708b9f15766987e6f17930dc18efc7a08
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2017-12-20 01:01:01 -0300

    corregidos mensajes de error

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,7 +3,7 @@ class Grade < ApplicationRecord
   belongs_to :evaluation
   validates :student , uniqueness: {scope: :evaluation, message:"A student can't have two notes for the same evaluation"}
   validates_each :grade do |record, attr, value|
-    record.errors.add(attr, 'Must be a number or Absent') if (!(Float(value) != nil rescue false) and value!="Absent" and value!="absent" )
+    record.errors.add(attr, "must be a number or 'Absent'") if (!(Float(value) != nil rescue false) and value!="Absent" and value!="absent" )
   end
   
 
