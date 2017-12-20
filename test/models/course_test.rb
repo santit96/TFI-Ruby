7af6cd7ee86ab34e4eda0b3ed6bc0b79f3cc8bba
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2017-12-20 20:41:35 -0300

    testing

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -6,7 +6,32 @@ class CourseTest < ActiveSupport::TestCase
   # end
 
   test "should create course" do
+  	c=Course.new name:"Ruby" , year:2018
+  	assert_equal(true,c.save)
+  	assert_equal("Ruby",c.name)	
+  	assert_equal(2018,c.year)		
+  	c.destroy
+  end
+
+   test "should not create course, name and year must be unique" do
   	c=Course.new name:"Ruby" , year:2017
-  	assert_equal(c.save,true)	
+  	d=Course.new name:"Ruby" , year:2017
+  	c.save
+  	assert_equal(false,d.save)	
+  	d.destroy
+  	c.destroy
+  end
+  test "should not crete course with a year from the past" do
+  		c=Course.new name:"Chaul" , year:2014
+  		assert_equal(false,c.save)
+  end
+  test "should not destroy course with evaluations" do
+  	c=Course.new name:"no destruir" , year:2017
+  	c.save
+  	e=Evaluation.new name: "ev1" ,date:Date.today, min_grade:4 , course_id:c.id
+  	e.save
+  	assert_equal(c.destroy,false)	
+  	e.destroy
+  	c.destroy
   end
 end
