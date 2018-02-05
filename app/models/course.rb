2444bacc8a7a4e805549ad6288daf1cc4963632e
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2018-02-05 17:26:38 -0300

    casi terminados los tests

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,5 @@ class Course < ApplicationRecord
 	has_many :evaluations , dependent: :restrict_with_error
 	validates :name ,presence: true , uniqueness: {scope: :year , message: "Shouldn't be the same course twice in a year"} 
 	validates :year ,presence: true , numericality: {only_integer:true , greater_than: 1.years.ago.year}
+
 end
--- test/controllers/grades_controller_test.rb
+++ test/controllers/grades_controller_test.rb
@@ -3,47 +3,41 @@ require 'test_helper'
 class GradesControllerTest < ActionDispatch::IntegrationTest
   
   setup do
+    sign_in users(:one)
+    @course = courses(:one)
+    @evaluation= evaluations(:one)
     @grade = grades(:one)
+    @student = students(:five)
   end
 
   test "should get index" do
-    get grades_url
+    get grades_course_path(@course)
     assert_response :success
   end
 
   test "should get new" do
-    get new_grade_url
+    get new_course_evaluation_grade_path(@course,@evaluation)
     assert_response :success
   end
 
   test "should create grade" do
     assert_difference('Grade.count') do
-      post grades_url, params: { grade: { evaluation_id: @grade.evaluation_id, grade: @grade.grade, student_id: @grade.student_id } }
+      post course_evaluation_grades_path(@course,@evaluation, grade: { evaluation_id: @grade.evaluation_id, grade: @grade.grade, student_id: @student.id })
     end
 
-    assert_redirected_to grade_url(Grade.last)
+    assert_redirected_to course_evaluation_grade_path(@course,@evaluation)
   end
 
-  test "should show grade" do
-    get grade_url(@grade)
-    assert_response :success
-  end
 
   test "should get edit" do
-    get edit_grade_url(@grade)
+    get edit_course_evaluation_grade_path(@course,@evaluation,@grade)
     assert_response :success
   end
 
   test "should update grade" do
-    patch grade_url(@grade), params: { grade: { evaluation_id: @grade.evaluation_id, grade: @grade.grade, student_id: @grade.student_id } }
-    assert_redirected_to grade_url(@grade)
+    patch course_evaluation_grade_path(@course,@evaluation,@grade), params: { grade: { evaluation_id: @grade.evaluation_id, grade: @grade.grade, student_id: @grade.student_id } }
+    assert_redirected_to course_evaluation_grade_path(@course,@evaluation)
   end
 
-  test "should destroy grade" do
-    assert_difference('Grade.count', -1) do
-      delete grade_url(@grade)
-    end
 
-    assert_redirected_to grades_url
-  end
 end
